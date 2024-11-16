bb63d5e9f9219482a24985fef035ba39afd13741
muthuselvam73 <muthuselvam73.bit@gmail.com>
2024-11-17 03:03:21 +0530

    Add files via upload

---
+++ src/models/adduserModel.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import db from '../db/db.js';
+
+const adduserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: String,
+  },{timestamps:true}
+  );
+  
+  const AddUser =db.model('addusers', adduserSchema);
+  export default AddUser
