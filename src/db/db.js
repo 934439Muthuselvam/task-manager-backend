311df9e9bbdc1b72b9af66034e75e4f77738d526
muthuselvam73 <muthuselvam73.bit@gmail.com>
2024-11-17 00:12:24 +0530

    updated

--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 
 
-const db=mongoose.createConnection("mongodb://localhost:27017/Users_data");
+const db=mongoose.createConnection("mongodb+srv://muthuselvam73bitf:1234@cluster0.0jr7f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 db.once('open',()=>console.log("db successfully connnneted") );
 db.on('error',(err)=>console.log(`db not connect ${err.message}`));
 
 export default db;
+
+// muthuselvam73bitasd
+// CxGkwV4qMVU7RvDc
