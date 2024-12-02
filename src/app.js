c1d09ab14e8b623bdbf5c93120b550bce74ed76b
934439Muthuselvam <142088293+934439Muthuselvam@users.noreply.github.com>
2024-12-03 03:50:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,7 @@ const app=express();
 app.use(express.json())
 
 app.use(morgan('tiny'))
-app.use(cors({
-  origin:["https://task-manager-frontend-ivory-delta.vercel.app/"],
-  credentials:true
-}));
+app.use(cors());
 app.use(router);
 
 app.get('/',(req,res)=> res.send("server is running"));
